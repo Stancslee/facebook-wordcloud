30223e2f86d915f1ff1a15eab5bc679dad973246
Justin Ly <justinsongly@gmail.com>
2019-03-02 16:56:03 -0800

    Read json file into pandas table

--- process_messages.py
+++ process_messages.py
@@ -2,13 +2,26 @@
 """
 Justin Ly
 """
+import json
+import pandas as pd
 
 def main():
     """
     1. Read in the .json file containing messages
-    2. Use the Counter object to keep counts of all words
+    2. Use pandas tables to save data
     3. Write the most frequently used words to top_words.txt
     """
+
+    all_data = dict()
+    file_path = 'data/message.json'
+
+    with open(file_path, 'r') as f:
+        all_data = json.loads(f.read())
+
+    flip_tards = all_data['participants']
+    flip_tards = [x['name'] for x in flip_tards]
+    table = pd.DataFrame(all_data['messages'])
+    print(table)
 
 if __name__ == "__main__":
     main()
