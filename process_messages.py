dde4231ef3ba37ea9be4274a505a785024438221
Justin Ly <justin@Justins-MacBook-Pro.local>
2019-03-02 16:19:17 -0800

    Create script that will process messages

---
+++ process_messages.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Justin Ly
+"""
+
+
+def main():
+    """
+    1. Read in the .json file containing messages
+    2. Use the Counter object to keep counts of all words
+    3. Write the most frequently used words to top_words.txt
+    """
+
+if __name__ == "__main__":
+    main()
+
